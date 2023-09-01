ee1b4879aaceabb1bd5d6707bcf5cc79b73d31af
Bett Enock <bettenock968@gmail.com>
2023-09-01 11:25:39 -0700

    display books from api.

--- src/components/AddBook.js
+++ src/components/AddBook.js
@@ -1,48 +1,60 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
+import { v4 as uuidv4 } from 'uuid';
 import { useSelector, useDispatch } from 'react-redux';
-import { addBook } from '../Redux/books/bookSlice';
+import { postBooks, getBooks } from '../Redux/books/bookSlice';
 import BooksDisplay from './BooksDisplay';
 
 const AddBook = () => {
+  const bookList = useSelector((store) => store.books);
   const dispatch = useDispatch();
-  const [booksData, setBooksData] = useState({ title: '', author: '' });
 
-  const handleEvent = (e) => {
-    const { name, value } = e.target;
-    setBooksData((previousData) => ({
-      ...previousData,
-      [name]: value,
-    }));
+  const [title, setTitle] = useState('');
+  const [author, setAuthor] = useState('');
+  const [category, setCategory] = useState('');
+
+  useEffect(() => {
+    dispatch(getBooks('books'));
+  }, [dispatch]);
+
+  const submitBooks = (e) => {
+    e.preventDefault();
+    const book = {
+      item_id: uuidv4(),
+      title,
+      author,
+      category,
+    };
+    dispatch(postBooks(book));
+    setTitle('');
+    setAuthor('');
+    setCategory('');
   };
 
-  const handleBooks = () => {
-    if (booksData.title && booksData.author) {
-      dispatch(addBook(booksData));
-      setBooksData({
-        title: '',
-        author: '',
-      });
-    }
-  };
-  const books = useSelector((state) => state.books);
+  const entries = Object.entries(bookList.books);
+
   return (
     <>
       <section>
-        <article className="">
-          {books.map((book) => (
+        <article className="book-co">
+          {entries.map(([key, book]) => (
             <BooksDisplay
-              key={book.item_id}
-              bookId={book.item_id}
-              title={book.title}
-              author={book.author}
+              key={key}
+              itemId={key}
+              book={book[0]}
             />
           ))}
         </article>
-        <form action="">
+        <form onSubmit={submitBooks}>
           <h3>Add New Book</h3>
-          <input name="title" type="text" placeholder="Book title..." value={booksData.title} onChange={handleEvent} />
-          <input name="author" type="text" placeholder="Book author..." value={booksData.author} onChange={handleEvent} />
-          <button type="button" className="add-btn" onClick={handleBooks}>Add Book</button>
+          <input type="text" id="title" value={title} placeholder="Book title..." required onChange={(e) => { setTitle(e.target.value); }} />
+          <input type="text" id="author" value={author} placeholder="Book author..." required onChange={(e) => { setAuthor(e.target.value); }} />
+          <select onChange={(e) => setCategory(e.target.value)} className="all-categories">
+            <option>Category</option>
+            <option>Romance</option>
+            <option>Business</option>
+            <option>Philosophy</option>
+          </select>
+          <button type="submit" className="add-btn">Add Book</button>
         </form>
       </section>
     </>

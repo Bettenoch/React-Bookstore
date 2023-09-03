52eade2b227296ccec7647030c4b2c66810055db
Bett Enock <bettenock968@gmail.com>
2023-09-02 19:10:48 -0700

    import navbar.css and add other page components.

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,22 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import '../styles/Navbar.css';
+// eslint-disable-next-line import/no-extraneous-dependencies
+import 'boxicons';
 
 const Navbar = () => (
-  <nav className="nav-items">
-    <div className="logo">Bookstore CMS</div>
-    <ul className="nav-links">
-      <li><Link className="navlink" to="/">Books</Link></li>
-      <li><Link className="navlink" to="/Categories">Categories</Link></li>
+  <nav className="nav-container">
+    <ul className="nav-items">
+      <li className="nav-right">
+        <div className="logo">Bookstore CMS</div>
+        <ul className="nav-links">
+          <li><Link className="navlink" to="/">Books</Link></li>
+          <li><Link className="navlink org" to="/Categories">Categories</Link></li>
+        </ul>
+      </li>
+      <li className="user">
+        <box-icon type="solid" name="user" color="#0290ff" />
+      </li>
     </ul>
   </nav>
 );
